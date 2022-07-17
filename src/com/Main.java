7e87291fce9c481320cf0ba04bc835249152c9d0
tpotur <tpotur@gmail.com>
2022-07-17 17:28:05 +0300

    Working first example

--- src/com/Main.java
+++ src/com/Main.java
@@ -2,6 +2,17 @@ package com;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Initial Commit");
+        System.out.println("************************** PlanetTravel 1 ****************************");
+        UIPlanetTravel uiPlanetTravel1 = new UIPlanetTravel("src/com/public_file_1");
+        uiPlanetTravel1.printPlanetsExitInfo();
+        System.out.println("1 " + uiPlanetTravel1.planetListInfo());
+        System.out.println("1 Count:" + uiPlanetTravel1.getAbsoluteVisitCount());
+
+
+        System.out.println("************************** PlanetTravel 2 ****************************");
+        UIPlanetTravel uiPlanetTravel2 = new UIPlanetTravel("src/com/public_file_2");
+        uiPlanetTravel2.printPlanetsExitInfo();
+        System.out.println("1 " + uiPlanetTravel2.planetListInfo());
+        System.out.println("2 Count:" + uiPlanetTravel2.getAbsoluteVisitCount());
     }
 }
